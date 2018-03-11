88ad7b6c7907c86a5dec1190c3325fbcbd8a0d53
vipulyadav150 <vipulyadav150@gmail.com>
2018-03-11 15:47:30 +0530

    Difference between instance and class Variables

---
+++ class_and_instance_variables.py
@@ -0,0 +1,15 @@
+class Girl:
+
+    gender = 'female'
+
+
+    def __init__(self , name):
+        self.name = name
+
+a = Girl('Anjali')
+b = Girl('Sushma')
+
+print(a.gender)
+print(b.gender)
+print(a.name)
+print(b.name)
